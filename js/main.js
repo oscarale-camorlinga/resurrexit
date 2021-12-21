d43d0fccd7b6c20d155bd6d24a16c74065477040
oscarale-camorlinga <oscarale.camorlinga@gmail.com>
2021-12-21 16:04:49 -0600

    Changed hw back button functionality
    Need to test on mobile. Will be deploying

--- js/main.js
+++ js/main.js
@@ -48,6 +48,16 @@ const footer = document.querySelector("footer");
 
 /*----------------------------- EVENT LISTENERS -----------------------------*/
 
+window.addEventListener("load", function() { // hw back button goes to home
+	window.history.pushState({ noBackExitsApp: true }, '');
+});
+
+window.addEventListener("popstate", function(e) { // hw back button goes to home
+	if(e.state && e.state.noBackExitsApp) {
+		window.history.pushState({ noBackExitsApp: true }, '');
+	}
+});
+
 receiver.addEventListener("message", function(e) {
 	if(footer) footer.innerHTML = e.data;
 });
